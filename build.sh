55b05982af1fce92c7ff7eb7104629c27c62e5c7
shadowyang <shadowyang@tencent.com>
2024-06-16 16:37:14 +0000

    Update

--- build.sh
+++ build.sh
@@ -1,85 +1,88 @@
-#! /bin/bash
+#!/bin/bash
+ 
+echo "NDK_PATH is $NDK_PATH"
+TOOLCHAIN=clang
+ANDROID_VERSION=14
 
-build_libjpeg_turbo(){
-    mkdir "out/$1"
-    
-    SYSROOT=${NDK_PATH}/platforms/android-${ANDROID_VERSION}/arch-$1
-    
-    if [ $1 = "arm" ]; then
-        HOST=arm-linux-androideabi
-        TOOLCHAIN=${NDK_PATH}/toolchains/$HOST-${TOOLCHAIN_VERSION}/prebuilt/${BUILD_PLATFORM}
-        ANDROID_CFLAGS="-march=armv7-a -mfloat-abi=softfp -fprefetch-loop-arrays -mfpu=neon -mthumb -D__ANDROID__ -D__ARM_ARCH_7__  --sysroot=${SYSROOT}"
-    elif [ $1 = "arm64" ]; then
-        HOST=aarch64-linux-android
-        TOOLCHAIN=${NDK_PATH}/toolchains/$HOST-${TOOLCHAIN_VERSION}/prebuilt/${BUILD_PLATFORM}
-        ANDROID_CFLAGS="--sysroot=${SYSROOT}"
-    elif [ $1 = "x86" ]; then
-        HOST=i686-linux-android
-        TOOLCHAIN=${NDK_PATH}/toolchains/$1-${TOOLCHAIN_VERSION}/prebuilt/${BUILD_PLATFORM}
-        ANDROID_CFLAGS="--sysroot=${SYSROOT}"
-    elif [ $1 = "x86_64" ]; then
-        HOST=x86_64-linux-android
-        TOOLCHAIN=${NDK_PATH}/toolchains/$1-${TOOLCHAIN_VERSION}/prebuilt/${BUILD_PLATFORM}
-        ANDROID_CFLAGS="--sysroot=${SYSROOT}"
-    fi
-    
-    export CPP=${TOOLCHAIN}/bin/${HOST}-cpp
-    export AR=${TOOLCHAIN}/bin/${HOST}-ar
-    export NM=${TOOLCHAIN}/bin/${HOST}-nm
-    export CC=${TOOLCHAIN}/bin/${HOST}-gcc
-    export LD=${TOOLCHAIN}/bin/${HOST}-ld
-    export RANLIB=${TOOLCHAIN}/bin/${HOST}-ranlib
-    export OBJDUMP=${TOOLCHAIN}/bin/${HOST}-objdump
-    export STRIP=${TOOLCHAIN}/bin/${HOST}-strip
-    
-    cd "libjpeg-turbo-1.5.3"
-    make clean
-    sh configure --host=${HOST} \
-      CFLAGS="${ANDROID_CFLAGS} -O3 -fPIE" \
-      CPPFLAGS="${ANDROID_CFLAGS}" \
-      LDFLAGS="${ANDROID_CFLAGS} -pie" --with-simd --with-jpeg8 ${2+"$@"} 
-    make
-    
-    cp .libs/*.a "../out/$1/"
-    cp .libs/*.so "../out/$1/"
-    cd ../
-}
+cd libjpeg-turbo
 
-usage(){
-    echo "usage: sh build.sh arm/arm64/x86/x86_64/all"
-    exit 1
-}
+mkdir build
+cd build
 
-NDK_PATH=$NDK_PATH
-BUILD_PLATFORM=linux-x86_64
-TOOLCHAIN_VERSION=4.9
-ANDROID_VERSION=21
+mkdir armeabi-v7a
+cd armeabi-v7a
 
-if [ -z $NDK_PATH ] || [ ! -d $NDK_PATH ] || [ ! -f "$NDK_PATH/ndk-build" ]; then
-    echo "\$NDK_PATH is not correct!"
-    exit 1
-fi
+echo "Build armeabi-v7a arch..."
 
-if [ $# -lt 1 ]; then
-    usage
-fi
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=armeabi-v7a \
+  -DCMAKE_INSTALL_PREFIX=../../../out/armeabi-v7a \
+  -DENABLE_STATIC=TRUE \
+  -DANDROID_ARM_MODE=arm \
+  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_ASM_FLAGS="--target=arm-linux-androideabi${ANDROID_VERSION}" \
+  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+  ../../
+make
+make install
+# make clean
 
-if [ ! -d "out" ]; then
-    mkdir "out"
-fi
+cd ../
+mkdir arm64-v8a
+cd arm64-v8a
 
-if [ $1 = "arm" ] || [ $1 = "arm64" ] || [ $1 = "x86" ] || [ $1 = "x86_64" ]; then
-    build_libjpeg_turbo $1
-    echo "build success, output dir is out/$1"
-elif [ $1 = "all" ]; then
-    build_libjpeg_turbo "arm"
-    build_libjpeg_turbo "arm64"
-    build_libjpeg_turbo "x86"
-    build_libjpeg_turbo "x86_64"
-    echo "build success, output dir is out"
-else
-    usage
-fi
+echo "Build arm64-v8a arch..."
 
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=arm64-v8a \
+  -DCMAKE_INSTALL_PREFIX=../../../out/arm64-v8a \
+  -DENABLE_STATIC=TRUE \
+  -DANDROID_ARM_MODE=arm \
+  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_VERSION}" \
+  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+  ../../
+  
+make
+make install
 
+cd ../
+mkdir x86
+cd x86
 
+echo "Build x86 arch..."
+
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=x86 \
+  -DCMAKE_INSTALL_PREFIX=../../../out/x86 \
+  -DENABLE_STATIC=TRUE \
+  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+  ../../
+
+make
+make install
+#make clean
+
+cd ../
+mkdir x86_64
+cd x86_64
+
+echo "Build x86_64 arch..."
+
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=x86_64 \
+  -DCMAKE_INSTALL_PREFIX=../../../out/x86_64 \
+  -DENABLE_STATIC=TRUE \
+  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+  ../../
+
+make
+make install
+
+echo "Build complete."
